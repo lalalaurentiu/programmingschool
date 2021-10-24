38ceed67705588ccad4d56fe82b8751acdf0176f
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2021-10-24 14:47:54 +0300

    rezolvare exercitiu si aranjare elemente

--- lessons/views.py
+++ lessons/views.py
@@ -25,7 +25,7 @@ def lesson(request, slug = None):
         lessons_content = lessons_content.filter(category=lesson_name)
         if str(lesson_name.category) == "Python":
             exercise = Exercise.objects.filter(category = lesson_name)
-            return render(request, 'lessons/python_leson.html', {'lessons_content': lessons_content, 'lessons_name':lesson_name, 'categories':categories, 'exercise':exercise})
+            return render(request, 'lessons/python_leson.html', {'lessons_content': lessons_content.order_by("id"), 'lessons_name':lesson_name, 'categories':categories, 'exercise':exercise})
         else:
             return render(request, 'lessons/html_leson.html', {'lessons_content': lessons_content, 'lessons_name':lesson_name, 'categories':categories})
 
