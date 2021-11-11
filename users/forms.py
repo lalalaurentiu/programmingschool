740ae31932850c5fa1ec73dfed9712b926e4c8a2
ManuelDenis <86168973+ManuelDenis@users.noreply.github.com>
2021-11-11 08:35:09 +0200

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -2,10 +2,38 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
 
+class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Username',
+                'background': '#01706c'
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nume si Prenume'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Telefon'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': "form-control",
+                'placeholder': 'Email'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-control',
+            }),
+        }
+
+
+
 
