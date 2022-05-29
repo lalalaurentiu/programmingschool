a3d7bcf4d70889512a5487cb7e83703039cddc75
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-05-29 16:21:24 +0300

    restilizare formular de adaugare lectii

--- lessons/forms.py
+++ lessons/forms.py
@@ -19,6 +19,16 @@ class LessonsAddForm(forms.ModelForm):
     class Meta:
         model = Lessons
         exclude = ("category", "slug")
+        widgets = {
+            "title":forms.TextInput(attrs={
+                "class":"form-control"
+            }),
+            "content":forms.Textarea(attrs={
+                "cols":10, 
+                "rows":3,
+                "class":"form-control md-textarea"
+            })
+        }
 
 class LessonForm(forms.ModelForm):
     class Meta:
